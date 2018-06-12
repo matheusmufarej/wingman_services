8613af0e3d99e4635583528885feff859b8ac745
Raghu Betina <raghu@firstdraft.com>
2018-06-12 05:28:21 +0000

    Direct association between openers and approaches

--- app/models/opener.rb
+++ app/models/opener.rb
@@ -1,6 +1,9 @@
 class Opener < ApplicationRecord
   # Direct associations
 
+  has_many   :approaches,
+             :dependent => :destroy
+
   belongs_to :styles,
              :class_name => "Style"
 
