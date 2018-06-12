be704854c4ba549993ae91772dcd779ed5abec28
Raghu Betina <raghu@firstdraft.com>
2018-06-12 05:28:21 +0000

    Indirect association between users and venues

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :venues,
+             :through => :approaches,
+             :source => :venue
+
   has_many   :openers,
              :through => :approaches,
              :source => :opener
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -8,6 +8,10 @@ class Venue < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :users,
+             :through => :approaches,
+             :source => :user
+
   # Validations
 
 end
