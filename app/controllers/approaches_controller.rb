ac6d0886724723cc2a2e4230e9e2d57423997a30
Raghu Betina <raghu@firstdraft.com>
2018-06-12 05:29:15 +0000

    Only approach user can access show page

--- app/controllers/approaches_controller.rb
+++ app/controllers/approaches_controller.rb
@@ -1,5 +1,5 @@
 class ApproachesController < ApplicationController
-  before_action :current_user_must_be_approach_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_approach_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_approach_user
     approach = Approach.find(params[:id])
